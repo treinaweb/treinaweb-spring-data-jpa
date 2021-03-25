9f6609bafc4819360d39a67d50c756365de744a0
Cleyson Lima <cleysonph@gmail.com>
2021-03-25 18:48:02 -0300

    03:05 - criado as actions para cadastrar e exibir os clientes

---
+++ src/main/java/br/com/treinaweb/twprojetos/controles/ClienteControle.java
@@ -0,0 +1,53 @@
+package br.com.treinaweb.twprojetos.controles;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.treinaweb.twprojetos.entidades.Cliente;
+import br.com.treinaweb.twprojetos.repositorios.ClienteRepositorio;
+
+@Controller
+@RequestMapping("/clientes")
+public class ClienteControle {
+
+    @Autowired
+    private ClienteRepositorio clienteRepositorio;
+
+    @GetMapping
+    public ModelAndView home() {
+        ModelAndView modelAndView = new ModelAndView("cliente/home");
+
+        modelAndView.addObject("clientes", clienteRepositorio.findAll());
+
+        return modelAndView;
+    }
+
+    @GetMapping("/cadastrar")
+    public ModelAndView cadastrar() {
+        ModelAndView modelAndView = new ModelAndView("cliente/formulario");
+
+        modelAndView.addObject("cliente", new Cliente());
+
+        return modelAndView;
+    }
+
+    @PostMapping("/cadastrar")
+    public String salvar(Cliente cliente) {
+        clienteRepositorio.save(cliente);
+
+        return "redirect:/clientes";
+    }
+
+    @GetMapping("/{id}/excluir")
+    public String excluir(@PathVariable Long id) {
+        clienteRepositorio.deleteById(id);
+
+        return "redirect:/clientes";
+    }
+    
+}
