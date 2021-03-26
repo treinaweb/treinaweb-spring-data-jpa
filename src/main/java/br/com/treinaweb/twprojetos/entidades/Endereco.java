8ba964d06e40150bc9fdb3e5acbc1be83f21bd44
Cleyson Lima <cleysonph@gmail.com>
2021-03-25 21:04:00 -0300

    04:07 - criado o enum UF e a entidade Endereco

---
+++ src/main/java/br/com/treinaweb/twprojetos/entidades/Endereco.java
@@ -0,0 +1,187 @@
+package br.com.treinaweb.twprojetos.entidades;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 2)
+    @Enumerated(EnumType.STRING)
+    private UF uf;
+
+    @Column(nullable = false)
+    private String cidade;
+
+    @Column(nullable = false)
+    private String bairro;
+
+    @Column(nullable = false)
+    private String logradouro;
+
+    @Column(nullable = false)
+    private String cep;
+
+    @Column(nullable = false)
+    private String numero;
+
+    private String complemento;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public UF getUf() {
+        return uf;
+    }
+
+    public void setUf(UF uf) {
+        this.uf = uf;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((bairro == null) ? 0 : bairro.hashCode());
+        result = prime * result + ((cep == null) ? 0 : cep.hashCode());
+        result = prime * result + ((cidade == null) ? 0 : cidade.hashCode());
+        result = prime * result + ((complemento == null) ? 0 : complemento.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((logradouro == null) ? 0 : logradouro.hashCode());
+        result = prime * result + ((numero == null) ? 0 : numero.hashCode());
+        result = prime * result + ((uf == null) ? 0 : uf.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Endereco other = (Endereco) obj;
+        if (bairro == null) {
+            if (other.bairro != null)
+                return false;
+        } else if (!bairro.equals(other.bairro))
+            return false;
+        if (cep == null) {
+            if (other.cep != null)
+                return false;
+        } else if (!cep.equals(other.cep))
+            return false;
+        if (cidade == null) {
+            if (other.cidade != null)
+                return false;
+        } else if (!cidade.equals(other.cidade))
+            return false;
+        if (complemento == null) {
+            if (other.complemento != null)
+                return false;
+        } else if (!complemento.equals(other.complemento))
+            return false;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (logradouro == null) {
+            if (other.logradouro != null)
+                return false;
+        } else if (!logradouro.equals(other.logradouro))
+            return false;
+        if (numero == null) {
+            if (other.numero != null)
+                return false;
+        } else if (!numero.equals(other.numero))
+            return false;
+        if (uf != other.uf)
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder enderecoCompleto = new StringBuilder();
+        enderecoCompleto.append(logradouro)
+                        .append(", n° ")
+                        .append(numero)
+                        .append(", ")
+                        .append(complemento)
+                        .append(" - ")
+                        .append(bairro)
+                        .append(". ")
+                        .append(uf.getDescricao())
+                        .append(" - ")
+                        .append(cidade)
+                        .append(". CEP: ")
+                        .append(cep);
+
+        return enderecoCompleto.toString();
+    }
+    
+}
