a98ee7bb3024b9dec090e8d72f9cd1f9bfd58bf6
Cleyson Lima <cleysonph@gmail.com>
2021-03-26 23:47:39 -0300

    07:17 - criado a entidade Projeto e o repositorio ProjetoRepositorio

---
+++ src/main/java/br/com/treinaweb/twprojetos/entidades/Projeto.java
@@ -0,0 +1,132 @@
+package br.com.treinaweb.twprojetos.entidades;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.NumberFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+import org.springframework.format.annotation.NumberFormat.Style;
+
+@Entity
+public class Projeto extends Entidade {
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String descricao;
+
+    @Column(name = "data_inicio", nullable = false)
+    @DateTimeFormat(iso = ISO.DATE)
+    private LocalDate dataInicio;
+
+    @Column(name = "data_fim")
+    @DateTimeFormat(iso = ISO.DATE)
+    private LocalDate dataFim;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id_fk", nullable = false)
+    private Cliente cliente;
+
+    @ManyToOne
+    @JoinColumn(name = "lider_id_fk", nullable = false)
+    private Funcionario lider;
+
+    @Column(nullable = false)
+    @NumberFormat(style = Style.CURRENCY, pattern = "#,##0.00")
+    private BigDecimal orcamento;
+
+    @Column(nullable = false)
+    @NumberFormat(style = Style.CURRENCY, pattern = "#,##0.00")
+    private BigDecimal gastos;
+
+    @ManyToMany
+    @JoinTable(
+        name = "projeto_funcionario",
+        joinColumns = @JoinColumn(name = "projeto_id_fk"),
+        inverseJoinColumns = @JoinColumn(name = "funcionario_id_fk")
+    )
+    private List<Funcionario> equipe;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(LocalDate dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public LocalDate getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(LocalDate dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Funcionario getLider() {
+        return lider;
+    }
+
+    public void setLider(Funcionario lider) {
+        this.lider = lider;
+    }
+
+    public BigDecimal getOrcamento() {
+        return orcamento;
+    }
+
+    public void setOrcamento(BigDecimal orcamento) {
+        this.orcamento = orcamento;
+    }
+
+    public BigDecimal getGastos() {
+        return gastos;
+    }
+
+    public void setGastos(BigDecimal gastos) {
+        this.gastos = gastos;
+    }
+
+    public List<Funcionario> getEquipe() {
+        return equipe;
+    }
+
+    public void setEquipe(List<Funcionario> equipe) {
+        this.equipe = equipe;
+    }
+    
+}
